1e849d5f8b0537445f3fbffb0e1064feb0d3a629
Muhammad Adam Luqman <muhammadadamluqman@graduate.utm.my>
2025-08-15 22:01:32 +0800

    Remove SSR and Netlify Angular Runtime for static deployment

--- @types/netlify__angular-runtime/index.d.ts
+++ @types/netlify__angular-runtime/index.d.ts
@@ -1,3 +1,6 @@
+declare module '@netlify/angular-runtime/common-engine.mjs' {
+  export function render(engine: any): Promise<any>;
+}
 declare module '@netlify/angular-runtime/server' {
   export function createServer(options: any): any;
   export function createViteServer(): any;
